860099a65387da8cf261968a311a12fb66008b5f
Kailynnelopez <kailynnelopez@gmail.com>
2022-12-05 08:49:53 -0800

    Connect to tailwind and made functional on port

---
+++ postcss.config.jsx
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./index.html', "./src/**/*.{jsx,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                
+            }
+        },
+    },
+    plugins:[]
+
+}
