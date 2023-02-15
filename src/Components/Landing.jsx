b826c1585d735078501c5805f556d5e45df6bf95
Kailynnelopez <kailynnelopez@gmail.com>
2023-02-14 20:42:34 -0800

    updated size

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -12,7 +12,7 @@ function Landing() {
         <div className="header">
           <h1>PA</h1>
           <h2>CE</h2>
-          <h3>a reading planner for planners & procrastinators alike</h3>
+          <h3>a reading planner for procrastinators & planners alike</h3>
           <img src={arrow} alt="down arrow"/>
         </div>
       </section>
